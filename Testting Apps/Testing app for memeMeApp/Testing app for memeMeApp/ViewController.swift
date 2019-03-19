157f153273d8ae78107a5c9c65f9685f0dfab251
Jose Ramos <jmrp.04@gmail.com>
2019-03-19 17:16:09 -0400

    Almost finish the memeMe app. Just need to add the navication var with save and share

--- Testting Apps/Testing app for memeMeApp/Testing app for memeMeApp/ViewController.swift
+++ Testting Apps/Testing app for memeMeApp/Testing app for memeMeApp/ViewController.swift
@@ -13,12 +13,19 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var pickImageButton: UIButton!
-    
     @IBOutlet var topTextField: UITextField!
-    
     @IBOutlet weak var bottomTextField: UITextField!
-    
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet var ImageSelector: UIToolbar!
+    
+    struct Meme {
+        let topText: String!
+        let bottomText: String!
+        let originalImage: UIImage!
+        let memedImage: UIImage!
+    }
+    
+    var meme: Meme!
     
     let pickerController = UIImagePickerController()
     
@@ -27,25 +34,77 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         self.imagePickerView.contentMode = UIView.ContentMode.scaleAspectFit
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
+        subscribeToKeyboardNotifications()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        imagePickerView.frame = CGRect(x: 0, y: 0, width: Int(self.view.frame.width), height: 600)
-        imagePickerView.backgroundColor = UIColor.white
-        imagePickerView.center = self.view.center
+        self.view.backgroundColor = UIColor.black
+        
+        imagePickerView.backgroundColor = UIColor.black
+        imagePickerView.translatesAutoresizingMaskIntoConstraints =  false
+        //imagePickerView.frame = CGRect(x: 0, y: 0, width: Int(self.view.frame.width) - 50, height: (4 * Int(self.view.frame.height))/5)
+        imagePickerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.8).isActive = true
+        imagePickerView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        imagePickerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        imagePickerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        imagePickerView.bottomAnchor.constraint(equalTo: self.ImageSelector.topAnchor).isActive = true
+        //imagePickerView.center = self.view.center
+        imagePickerView.contentMode = .scaleAspectFit
         
         self.pickerController.delegate = self
         
         self.topTextField.delegate = self
         self.bottomTextField.delegate = self
         
-        self.topTextField.textFieldLayout(view: self.view, yPosition: 100, text: "TOP", placeholder: "", topOrBottomPosition: "top")
+        self.topTextField.memeTextFieldLayoutAndAttributes(view: self.view, text: "TOP", placeholder: "", topOrBottomPosition: "top")
         
-        self.bottomTextField.textFieldLayout(view: self.view, yPosition: 400, text: "BOTTOM", placeholder: "", topOrBottomPosition: "bottom")
+        self.bottomTextField.memeTextFieldLayoutAndAttributes(view: self.view, text: "BOTTOM", placeholder: "", topOrBottomPosition: "bottom")
         
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+    
+        if bottomTextField.isEditing {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
+        
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -60,7 +119,7 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
         
-        
+        self.dismiss(animated: true, completion: nil)
         
     }
     
@@ -78,39 +137,70 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func save() -> Meme {
+        // Create the meme
+        let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imagePickerView.image!, memedImage: generateMemedImage())
+        
+        return meme
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navbar
+        
+        return memedImage
+    }
     
 }
 
 extension UITextField {
     
-    func textFieldLayout(view: UIView, yPosition: Int, text: String, placeholder: String, topOrBottomPosition: String){
-        
-        let width = Int(view.frame.width)
-        var height = Int(view.frame.height)
+    func memeTextFieldLayoutAndAttributes(view: UIView, text: String, placeholder: String, topOrBottomPosition: String){
         
         if topOrBottomPosition == "top" {
             
-            height = (height/3)
+            //height = (height/4)
+            self.centerYAnchor.constraint(lessThanOrEqualTo: view.centerYAnchor, constant: CGFloat(-view.frame.width) / 4).isActive = true
+            self.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
             
         }
         else if topOrBottomPosition == "bottom" {
             
-            height = (1 * height) / 3
+            //height = ((3 * height) / 5)
+            self.centerYAnchor.constraint(greaterThanOrEqualTo: view.centerYAnchor, constant: CGFloat(view.frame.width) / 4).isActive = true
+            self.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            
         }
         
-        self.frame = CGRect(x: 0, y: yPosition, width: width, height: 60)
+        self.translatesAutoresizingMaskIntoConstraints = false
         
         self.text = text
         self.textColor = UIColor.white
         self.textAlignment = .center
-        self.font = UIFont(name: "GillSans-Bold", size: 40)
+        self.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)
         self.defaultTextAttributes[.strokeColor] = UIColor.black
         self.defaultTextAttributes[.strokeWidth] = -7
         self.borderStyle = .none
         self.placeholder = placeholder
         self.autocapitalizationType = .allCharacters
         self .autocorrectionType = .no
-        
     }
 }
